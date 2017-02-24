9a928508a132bf005543d1058217df88d81e20f9
hwj <997242980@qq.com>
2017-02-24 15:32:10 +0800

    黄文靖提交

--- laravel52/resources/views/market/market.blade.php
+++ laravel52/resources/views/market/market.blade.php
@@ -10,95 +10,21 @@
       <link href="{{asset('css/share.css')}}" rel="stylesheet"/>
       <link href="{{asset('css/index.css')}}" rel="stylesheet"/>
     <script type="text/javascript">
-    angular.module('ionicApp', ['ionic'])
-    .controller('SlideController', function($scope) {
-      $scope.myActiveSlide = 0;
+      
+        angular.module('ionicApp', ['ionic'])
+
+            .controller('SlideController', function($scope) {
+
+                $scope.myActiveSlide = 0;
+
+            });
+
     })
     </script>
       <style>
           .type ul li{float: left;list-style: none;margin-left: 20px; }
           .type ul{border: 1px solid #000; height: 30px; line-height: 30px; background:#2894FF;color:#fff;}
           a{ text-decoration:none}
-      </style>
-      <style type="text/css">
-          .lei {
-              position: relative;
-              display: inline-block;
-              width: 100%;
-          }
-
-          .classify_list {
-              display: block;
-              float: left;
-              width: 24.7%;
-              text-align: center;
-              cursor: pointer;
-              height: 40px;
-
-          }
-
-          .classify_list span {
-              display: inline-block;
-              width: 100%;
-              height: 40px;
-              line-height: 40px;
-          }
-
-          .classify_list .firstspan {
-              border-right: 1px solid #c0c0c0;
-              border-left: 1px solid #c0c0c0;
-          }
-          .list_ul {
-              position: relative;
-              left: 0;
-              text-align: left;
-              z-index: 10;
-              background-color: #f2f2f2;
-              display: none;
-          }
-          .list_ul li ,.list_ul li span{
-              height: 35px;
-              line-height:35px;
-              padding-left: 5px;
-              border-bottom: 0.05rem solid #dadada;
-          }
-
-          .sta {
-              background: #fff;
-          }
-
-          .lastUL {
-              width: 300%;
-              padding: 0 0.5rem;
-              background-color: #fff;
-              position: absolute;
-              top: 0;
-              left: 100%;
-              display: none;
-          }
-
-          .classify_list:nth-child(2) .list_ul li span {
-              border-right: none;
-              border-left: none;
-          }
-
-          .classify_list:nth-child(3) .list_ul li span {
-              border-right: none;
-              border-left: none;
-          }
-
-          .list_ul li a {
-              display: inline-block;
-              width: 100%;
-              text-align: left;
-          }
-
-          .showlist {
-              display: block;
-          }
-          #page ul li {
-              float: left;}
-
       </style>
   </head>
 <body>
@@ -203,8 +129,8 @@
     <a href="javascript:void(0)" class="more_r more" id="back-to-top">☟查看更多<i></i></a>
 </div>
 	<!-- 底部-->
-    @include('master')
   </body>
+    @include('master')
   <script src="{{asset('jquery-2.1.1.min.js')}}" type="text/javascript"></script>
 {{--  <script src="{{asset('style/js/ionic.bundle.min.js')}}"></script>--}}
   <script src="{{asset('js/all.js')}}"></script>
@@ -215,7 +141,6 @@
        $(".allType").click(function(){
             if( $("#typeList").css("display") == 'none')
             {
-                //$("#typeList").show();
                 $("#typeList").show();
             }else
             {
